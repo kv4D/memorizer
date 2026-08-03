255b071e7dc1b6c2c41a0d4abab429151d139150
withered <sergeyzezyanov1@gmail.com>
2026-08-04 00:24:44 +0700

    feat: add backend structure
    - used uv to work with packages
    - created modular structure
    - added empty files for using docker in the future
    - added an example of a module (/users)

---
+++ backend/src/core/configs.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# path to the env file
+BASE_DIR = Path(__file__).resolve().parents[4]
+ENV_PATH = BASE_DIR / '.env'
+
+
+class APISettings(BaseSettings):
+    """API settings."""
+
+    TITLE: str = "FastAPI"
+    DESCRIPTION: str = "API for the Memorizer application"
+    VERSION: str = "0.1.0"
+    API_HOST: str = "127.0.0.1"
+    API_PORT: int = 8000
+    DEBUG_MODE: bool = True
+
+    model_config = SettingsConfigDict(env_file=ENV_PATH,
+                                      env_file_encoding='utf-8',
+                                      extra='ignore')
+
+
+# import these settings in other modules
+api_settings = APISettings()
---
+++ backend/src/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import uvicorn
+
+from users.api import users_router
+from core.configs import api_settings
+
+
+app = FastAPI(
+    title=api_settings.TITLE,
+    version=api_settings.VERSION,
+    description=api_settings.DESCRIPTION
+)
+
+# add routers here
+app.include_router(users_router)
+
+def main() -> None:
+    """The main function to run the FastAPI application."""
+    uvicorn.run(
+        "main:app",
+        host=api_settings.API_HOST,
+        port=api_settings.API_PORT,
+        reload=api_settings.DEBUG_MODE
+    )
+
+
+if __name__ == "__main__":
+    main()
